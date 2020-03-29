c30dbd18c1f49ab759133ec66290be837e803040
Prakash Acharya <pracharya2601@gmail.com>
2020-03-29 10:58:51 -0700

    port fix

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var PORT = process.env.PORT || 3000;
 var bodyParser=require("body-parser");
  
 // all environments
-app.set('port', process.env.PORT || 8080);
+app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
